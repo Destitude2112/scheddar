6766e0b461827983816cd971af489e71f3d7708c
Stephanie DeMane <sdemane34@gmail.com>
2013-05-07 03:36:03 -0400

    meetingpane

--- src/edu/brown/cs32/scheddar/gui/MeetingPane.java
+++ src/edu/brown/cs32/scheddar/gui/MeetingPane.java
@@ -5,6 +5,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.List;
 
+import javax.swing.Box;
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JList;
@@ -27,58 +29,43 @@ import edu.brown.cs32.scheddar.ScheddarTime;
 public class MeetingPane extends ScheddarSubPane {
 	private static final long serialVersionUID = 1L;
 	
-	ScheddarPane pane;
 	JTextField name;
-	JTextField time;
 	JList<String> groupsInvolved;
 	JTextField newGroup;
 	JTextField description;
 	JList<String> peopleAttending;
 	JTextField newAttendee;
-//	JTextField importanceThreshold;
 	
 	public MeetingPane(ScheddarPane s, Meeting m) {
 		super(s);
-		pane = s;
-		JPanel panel = new JPanel();
+		
 		name = new JTextField(20);
-		time = new JTextField(20);
-		groupsInvolved = new JList<String>();
-		newGroup = new JTextField(20);
-		description = new JTextField(20);
-		peopleAttending = new JList<String>();
-		newAttendee = new JTextField(20); 
-//		importanceThreshold = new JTextField(20);
+		description = new JTextField(40);
 		
-		if(m!=null) {
-			name.setText(m.getName());
-			description.setText(m.getDescription());
-//			importanceThreshold.setText(Double.toHexString(m.getImportanceThreshold()));
-		}
-		JButton save = new JButton("Save");
 		
-		JButton addGroup = new JButton("Add group");
-		addGroup.addActionListener(new ActionListener() {			
-			@Override
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
-
+		JPanel panel = new JPanel();
+		panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+		panel.add(Box.createVerticalStrut(20));
 		
-		panel.add(new JLabel("Name:"));
-		panel.add(name);
-		panel.add(new JLabel("Time:"));
-		panel.add(time);
-		panel.add(new JLabel("Groups:"));
-		panel.add(groupsInvolved);
-		panel.add(new JLabel("Description:"));
-		panel.add(description);
-		panel.add(new JLabel("Attendees:"));
-		panel.add(peopleAttending);
-		panel.add(new JLabel("Importance:"));
-//		panel.add(importanceThreshold);
-		panel.add(save);
-		add(panel);		
+		JPanel namePanel = new JPanel();
+		namePanel.add(new JLabel("Meeting Title:"));
+		namePanel.add(name);
+		panel.add(namePanel);
+		panel.add(Box.createVerticalStrut(10));
+		
+		JPanel descriPanel = new JPanel();
+		descriPanel.add(new JLabel("Description:"));
+		descriPanel.add(description);
+		panel.add(descriPanel);
+		panel.add(Box.createVerticalStrut(20));
+		
+		
+		
+		
+		
+		
+		
+		
 	}
 	
 	public Dimension getPreferredSize() {
