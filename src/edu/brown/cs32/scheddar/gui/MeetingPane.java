1c20d852a848bacad4b2f75da24728d2cb46a07d
aerlinn <sdemane34@gmail.com>
2013-05-08 05:43:57 -0400

    more meetingpane

--- src/edu/brown/cs32/scheddar/gui/MeetingPane.java
+++ src/edu/brown/cs32/scheddar/gui/MeetingPane.java
@@ -12,6 +12,7 @@ import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.DefaultListModel;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JPanel;
@@ -48,6 +49,11 @@ public class MeetingPane extends ScheddarSubPane {
 	JList<String> unaddedGroups;
 	JList<String> addedGroups;
 	
+	JComboBox<String> nameComboBox;
+	JComboBox<String> impComboBox;
+	
+	String[] importanceArray = {"Whatever", "Debatably Important", "Definitely Important", "Extremely Important", "Cataclysmically Important"};
+	
 	public MeetingPane(ScheddarPane s, Meeting m) {
 		super(s);
 		
@@ -58,6 +64,8 @@ public class MeetingPane extends ScheddarSubPane {
 		
 		// setting names and description fields
 		
+		// TODO: Make name a separate dialog box for each new meeting
+		// TODO: switch to GridBagLayout
 		name = new JTextField(meeting.getName(),20);
 		nameDoc = name.getDocument();
 		nameDoc.addDocumentListener(new DocumentListener() {
@@ -127,8 +135,10 @@ public class MeetingPane extends ScheddarSubPane {
 		unaddedGroups = new JList<String>();
 		unaddedGroups.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 		
-		updateGroupLists();
+		updateLists();
 		
+		
+		// TODO: add labels above scrollpanes
 		JScrollPane unaddedGroupsPane = new JScrollPane(unaddedGroups);
 		JScrollPane addedGroupsPane = new JScrollPane(addedGroups);
 		
@@ -136,26 +146,26 @@ public class MeetingPane extends ScheddarSubPane {
 		
 		JPanel groupButtonsPanel = new JPanel(new GridLayout(4,1,0,3));
 		
-		JButton addGroup = new JButton("Add Group to Meeting -->");
+		JButton addGroup = new JButton("Add to Meeting -->");
 		addGroup.addActionListener(new ActionListener() {
 			
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
 				if (unaddedGroups.getSelectedValue() != null) {
 					meeting.addGroupInvolved(_scheddar.getGroupFromName(unaddedGroups.getSelectedValue()));
-					updateGroupLists();
+					updateLists();
 				}
 			}
 		});
 		
-		JButton removeGroup = new JButton("<-- Remove Group from Meeting");
+		JButton removeGroup = new JButton("<-- Remove from Meeting");
 		removeGroup.addActionListener(new ActionListener() {
 			
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
 				if (addedGroups.getSelectedValue() != null) {
 					meeting.removeGroupInvolved(_scheddar.getGroupFromName(addedGroups.getSelectedValue()));
-					updateGroupLists();
+					updateLists();
 				}
 			}
 		});
@@ -171,11 +181,49 @@ public class MeetingPane extends ScheddarSubPane {
 		
 		
 		panel.add(groupPanel);
+		panel.add(Box.createVerticalStrut(20));
+		
+		JPanel individualPanel = new JPanel(new GridLayout(1,3,8,0));
+		nameComboBox = new JComboBox<String>();
+		nameComboBox.setEditable(true);
+		impComboBox = new JComboBox<String>(importanceArray);
+		impComboBox.setSelectedIndex(2);
+		JButton addPersonButton = new JButton("Add to Meeting ^^");
+		addPersonButton.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				meeting.addExtraPerson(_scheddar.getPersonFromName((String)nameComboBox.getSelectedItem()), (impComboBox.getSelectedIndex() + 1) * 20);
+				updateLists();
+			}
+		});
+		
+		individualPanel.add(nameComboBox);
+		individualPanel.add(impComboBox);
+		individualPanel.add(addPersonButton);
+		panel.add(individualPanel);
+		panel.add(Box.createVerticalStrut(20));
+		
+		
+		
+		// submitting time ranges
+		JPanel timeRangePanel = new JPanel();
+		timeRangePanel.setLayout(new BoxLayout(timeRangePanel,BoxLayout.Y_AXIS));
+		JPanel durationPanel = new JPanel();
+		String[] durations = {"0:30", "1:00", "1:30", "2:00", "2:30", "3:00", "3:30", "4:00", "4:30", "5:00", "5:30", "6:00"};
+		JComboBox<String> durationBox = new JComboBox<String>(durations);
+		durationBox.setSelectedIndex(1);
+		durationPanel.add(new JLabel("Required meeting duration (H:MM):"));
+		durationPanel.add(durationBox);
+		timeRangePanel.add(durationPanel);
+		
+		// TODO: fill in with JFormattedTextFields (requires research)
+		
 		
 		add(panel);
 	}
 	
-	private void updateGroupLists() {
+	private void updateLists() {
 		DefaultListModel<String> addedList = new DefaultListModel<String>();
 		DefaultListModel<String> unaddedList = new DefaultListModel<String>();
 		
@@ -191,12 +239,14 @@ public class MeetingPane extends ScheddarSubPane {
 		
 		addedGroups.setModel(addedList);
 		unaddedGroups.setModel(unaddedList);
+		
+		//TODO: also update person list; add individuals to groups list (attendees)
 	}
 	
 	
 	public Dimension getPreferredSize() {
 		Dimension d = _scheddarPane.getPreferredSize();
-		return new Dimension(d.width * 3/4, d.height);
+		return new Dimension(d.width * 3/4 - 10, d.height);
 	}
 	
 	
